92478eaeeae17b9414ac0f719f017a855f8b7f14
dodadoa <maddodadoa@gmail.com>
2021-01-21 05:32:38 +0700

    add drawer for node

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,6 @@
-import * as React from 'react'
+import React, { useState } from 'react'
+import { Drawer } from 'antd'
+import 'antd/dist/antd.css'
 import ForceGraph2D from 'react-force-graph-2d'
 import data from '../dataset/index.json'
 import bg from '../images/bg.jpg'
@@ -12,9 +14,16 @@ const pageStyles = {
   width: '100%',
   backgroundSize: 'cover',
 }
-
-const handleClickNode = (x) => {
-  alert(x.id)
+const drawerContentContainerStyle = {
+  marginTop: '24px',
+  padding: '6px',
+}
+const drawerBodyStyle = {
+  backgroundColor: 'white',
+  color: 'black',
+}
+const drawerStyle = {
+  width: '400px',
 }
 
 const addedColorLinks = data.links.map((link) => ({ ...link, color: 'white', opacity: 0.5 }))
@@ -24,39 +33,62 @@ const addedColorLinkGraphData = {
   links: addedColorLinks,
 }
 
-const IndexPage = () => (
-  <main style={pageStyles}>
-    <title>Immortal are busy this day</title>
-    {
-      typeof window !== 'undefined' && (
-        <ForceGraph2D
-          onNodeClick={handleClickNode}
-          graphData={addedColorLinkGraphData}
-          nodeCanvasObject={(node, ctx, globalScale) => {
-            const label = node.id
-            const fontSize = 14 / globalScale
-            ctx.font = `${fontSize}px Sans-Serif`
-            const textWidth = ctx.measureText(label).width
-            const bckgDimensions = [textWidth, fontSize].map((n) => n + fontSize * 0.5)
-            ctx.shadowColor = 'rgba(255, 255, 255, 1)'
-            ctx.shadowBlur = 15
-            ctx.fillStyle = 'rgba(255, 255, 255, 1)'
-            ctx.fillRect(
-              node.x - bckgDimensions[0] / 2,
-              node.y - bckgDimensions[1] / 2,
-              ...bckgDimensions,
-            )
+const IndexPage = () => {
+  const [visible, setVisible] = useState(false)
+  const [dataNode, setDataNode] = useState({})
 
-            ctx.shadowBlur = 0
-            ctx.textAlign = 'center'
-            ctx.textBaseline = 'middle'
-            ctx.fillStyle = 'rgba(0, 0, 0, 1)'
-            ctx.fillText(label, node.x, node.y)
-          }}
-        />
-      )
-    }
-  </main>
-)
+  const handleClickNode = (x) => {
+    setVisible(true)
+    setDataNode(x)
+  }
+  const handleCloseDrawer = () => setVisible(false)
+
+  return (
+    <main style={pageStyles}>
+      <title>Immortal are busy this day</title>
+      <Drawer
+        visible={visible}
+        placement="right"
+        onClose={handleCloseDrawer}
+        mask={false}
+        style={drawerStyle}
+        bodyStyle={drawerBodyStyle}
+      >
+        <div style={drawerContentContainerStyle}>
+          {JSON.stringify(dataNode)}
+        </div>
+      </Drawer>
+      {
+        typeof window !== 'undefined' && (
+          <ForceGraph2D
+            onNodeClick={handleClickNode}
+            graphData={addedColorLinkGraphData}
+            nodeCanvasObject={(node, ctx, globalScale) => {
+              const label = node.id
+              const fontSize = 16 / globalScale
+              ctx.font = `${fontSize}px Sans-Serif`
+              const textWidth = ctx.measureText(label).width
+              const bckgDimensions = [textWidth, fontSize].map((n) => n + fontSize * 0.5)
+              ctx.shadowColor = 'rgba(255, 255, 255, 1)'
+              ctx.shadowBlur = 15
+              ctx.fillStyle = 'rgba(255, 255, 255, 1)'
+              ctx.fillRect(
+                node.x - bckgDimensions[0] / 2,
+                node.y - bckgDimensions[1] / 2,
+                ...bckgDimensions,
+              )
+
+              ctx.shadowBlur = 0
+              ctx.textAlign = 'center'
+              ctx.textBaseline = 'middle'
+              ctx.fillStyle = 'rgba(0, 0, 0, 1)'
+              ctx.fillText(label, node.x, node.y)
+            }}
+          />
+        )
+      }
+    </main>
+  )
+}
 
 export default IndexPage
