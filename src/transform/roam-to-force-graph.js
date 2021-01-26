8c45afe6b00e9252cc7aaafb86791d955fbe6aff
dodadoa <maddodadoa@gmail.com>
2021-01-26 18:38:14 +0700

    initiate the transformation of graph and tests

--- src/transform/roam-to-force-graph.js
+++ src/transform/roam-to-force-graph.js
@@ -1,4 +1,32 @@
-const transform = (x) => x
+const travel = (currentNode, links, nodes) => {
+  if (!currentNode.children) {
+    return { links, nodes: [...nodes, currentNode.uid && { id: currentNode.uid }] }
+  }
+  return travel(
+    currentNode.children,
+    [
+      ...links,
+      ...currentNode.children.map((child) => ({ source: currentNode.uid, target: child.uid })),
+    ],
+    [
+      ...nodes,
+      currentNode.uid && { id: currentNode.uid },
+      ...currentNode.children.map((child) => ({ id: child.uid })),
+    ],
+  )
+}
+
+const transform = (x) => {
+  if (x.length === 0) return []
+
+  return x.reduce((transformedGraph, currentNode) => {
+    const transformed = travel(currentNode, [], [])
+    return {
+      links: [...transformedGraph.links, ...transformed.links],
+      nodes: [...transformedGraph.nodes, ...transformed.nodes],
+    }
+  }, { links: [], nodes: [] })
+}
 
 export default {
   transform,
